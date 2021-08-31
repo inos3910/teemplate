889ffde0ac4d5c48adb23a649e0a9edd466c182f
Yosuke Inoue <ysk.inoue@sharesl.net>
2021-08-31 22:25:40 +0900

    [update] dart-sass調整

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -4,7 +4,7 @@ import { deleteCssDir, buildCss } from './tasks/css';
 import { deleteJsDistDir, buildJs, buildJsAll } from './tasks/js';
 import { deleteTsDistDir, buildTs } from './tasks/ts';
 import { browsersync } from './tasks/browsersync';
-import { imagemin, webp, sprite, spritemin } from './tasks/img';
+import { imagemin, webp } from './tasks/img';
 
 //Default
 exports.default = gulp.series(watcher, browsersync);
@@ -14,11 +14,9 @@ exports.build = gulp.series(
   deleteJsDistDir,
   buildCss,
   buildJsAll
-);
+  );
 //build TypeScript
 exports.buildTs = gulp.series(deleteTsDistDir, buildTs);
 //画像処理系
 exports.imagemin = imagemin;
 exports.webp = webp;
-exports.sprite = sprite;
-exports.spritemin = spritemin;
--- gulpfile.babel.js/tasks/img.js
+++ gulpfile.babel.js/tasks/img.js
@@ -7,8 +7,6 @@ import plumber from 'gulp-plumber';
 import gulpImagemin from 'gulp-imagemin';
 import pngquant from 'imagemin-pngquant';
 import mozjpeg from 'imagemin-mozjpeg';
-//CSSスプライト
-import spritesmith from 'gulp.spritesmith';
 //webp生成
 import gulpWebp from 'gulp-webp';
 import rename from 'gulp-rename';
@@ -54,48 +52,3 @@ function webp() {
   .pipe(gulp.dest(paths.imageDir));
 }
 exports.webp = webp;
-
-//CSSスプライト画像の生成
-function sprite() {
-  const spriteData = gulp.src(globs.sprite, {
-    allowEmpty : true,
-    since      : gulp.lastRun(sprite)
-  })
-  .pipe(spritesmith({
-    imgName   : 'dest/sprite.png',
-    cssName   : '_sprite.scss',
-    imgPath   : `../sprite/dest/sprite.png`,
-    cssFormat : 'scss',
-    cssVarMap : s => {
-      s.name = 'sprite-' + s.name;
-    }
-  }));
-  spriteData.img.pipe(gulp.dest(paths.spriteDir));
-  return spriteData.css.pipe(gulp.dest(`${paths.sassDir}/foundation`));
-}
-exports.sprite = sprite;
-
-//スプライト画像の圧縮
-function spritemin(){
-  return gulp
-  .src(globs.sprites,{
-    allowEmpty : true,
-    since      : gulp.lastRun(spritemin)
-  })
-  .pipe(plumber())
-  .pipe(gulpImagemin([
-    pngquant({
-      quality : [.8, .9],
-      speed   : 1,
-      floyd   : 0
-    }),
-    mozjpeg({
-      quality     : 90,
-      progressive : true
-    }),
-    gulpImagemin.optipng()
-    ])
-  )
-  .pipe(gulp.dest(paths.spriteminDir));
-}
-exports.spritemin = spritemin;
